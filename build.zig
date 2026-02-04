2b9c17cf6355dc9b0673d762c4d2058ab386fcde
haruki7049 <tontonkirikiri@gmail.com>
2026-02-04 14:15:39 +0900

    Create zig repo

---
+++ build.zig
@@ -0,0 +1,38 @@
+const std = @import("std");
+const l = @import("lightmix");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lightmix = b.dependency("lightmix", .{});
+    const lightmix_filters = b.dependency("lightmix_filters", .{});
+    const lightmix_synths = b.dependency("lightmix_synths", .{});
+    const lightmix_temperaments = b.dependency("lightmix_temperaments", .{});
+
+    const mod = b.createModule(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+        .imports = &.{
+            .{ .name = "lightmix", .module = lightmix.module("lightmix") },
+            .{ .name = "lightmix_filters", .module = lightmix_filters.module("lightmix_filters") },
+            .{ .name = "lightmix_synths", .module = lightmix_synths.module("lightmix_synths") },
+            .{ .name = "lightmix_temperaments", .module = lightmix_temperaments.module("lightmix_temperaments") },
+        },
+    });
+
+    const wave_step = try l.createWave(b, mod, .{
+        .func_name = "gen",
+        .wave = .{ .bits = 16, .format_code = .pcm },
+    });
+    b.getInstallStep().dependOn(wave_step);
+
+    // Unit tests
+    const unit_tests = b.addTest(.{ .root_module = mod });
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+
+    // Test step
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
+}
---
+++ flake.nix
@@ -0,0 +1,87 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        { pkgs, lib, ... }:
+        let
+          pretend-distortion = pkgs.stdenv.mkDerivation {
+            name = "pretend-distortion";
+            src = lib.cleanSource ./.;
+
+            nativeBuildInputs = [
+              pkgs.zig_0_15.hook
+            ];
+
+            postPatch = ''
+              ln -s ${pkgs.callPackage ./.deps.nix { }} $ZIG_GLOBAL_CACHE_DIR/p
+            '';
+          };
+        in
+        {
+          treefmt = {
+            projectRootFile = ".git/config";
+
+            # Nix
+            programs.nixfmt.enable = true;
+
+            # Zig
+            programs.zig.enable = true;
+
+            # TOML
+            programs.taplo.enable = true;
+
+            # GitHub Actions
+            programs.actionlint.enable = true;
+
+            # Markdown
+            programs.mdformat.enable = true;
+
+            # ShellScript
+            programs.shellcheck.enable = true;
+            programs.shfmt.enable = true;
+          };
+
+          packages = {
+            inherit pretend-distortion;
+            default = pretend-distortion;
+          };
+
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = [
+              # Compiler
+              pkgs.zig_0_15
+
+              # LSP
+              pkgs.nil
+              pkgs.zls
+
+              # Music Player
+              pkgs.sox # Use this command as: `play result.wav`
+
+              # Zon2nix
+              pkgs.zon2nix
+            ];
+          };
+        };
+    };
+}
